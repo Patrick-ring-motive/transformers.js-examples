8e2e3e23a69b978cdbc2dd492dadbac0c3a752e0
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-04-01 02:48:45 -0500

    style: js-beautify formatting (next-server/app/layout.js)

--- next-server/app/layout.js
+++ next-server/app/layout.js
@@ -25,11 +25,10 @@ export default function RootLayout({
     <
     body className = {
       `${geistSans.variable} ${geistMono.variable} antialiased`
-    } >
-    {
+    } > {
       children
     } <
-    /body> <
-    /html>
+    /body> < /
+    html >
   );
 }
